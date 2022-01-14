99c4f98ac53fb4f9367ff27695b57e15156b8171
Jef Kazimer <jef@jeftek.com>
2022-01-14 17:07:53 -0600

    Added Sample for Bulk Assignment

---
+++ PowerShell/IdentityGovernance/New-EMRequestsFromTextFile.ps1
@@ -0,0 +1,82 @@
+<#
+.SYNOPSIS
+    Sample Script to bulk direct assignments by a text file of email addresses
+.DESCRIPTION
+    Sample Script to bulk direct assignments by a text file of email addresses
+.EXAMPLE
+    New-EMRequestsFromTextFile -TextFile C:\temp\NewEmails.txt -AccessPackageName "External User Onboarding"
+.EXAMPLE
+    New-EMRequestsFromTextFile -TextFile C:\temp\NewEmails.txt
+.INPUTS
+    Inputs to this cmdlet (if any)
+.OUTPUTS
+    Output from this cmdlet (if any)
+.NOTES
+    SEe detailed documentation for using Entitlement Maanagement services using Microsoft Fraph SDK Powershell module at https://docs.microsoft.com/en-us/azure/active-directory/governance/entitlement-management-access-package-assignments
+
+#>
+function New-EMRequestsFromTextFile {
+    [CmdletBinding()]
+    param (
+        # Full Path to Text File of email addresses to directly assign to Entitlement Management Access Package (1 address per line)
+        [Parameter(Mandatory = $true,
+            Position = 0,
+            ValueFromPipeline = $true,
+            ValueFromPipelineByPropertyName = $true,
+            ValueFromRemainingArguments = $false, 
+            ParameterSetName = 'Default')]
+        [ValidateNotNull()]
+        [ValidateNotNullOrEmpty()]
+        $TextFilePath,
+        # Display Name of the Access Package for Assignment
+        [Parameter(Mandatory = $false,
+            Position = 0,
+            ValueFromPipeline = $true,
+            ValueFromPipelineByPropertyName = $true,
+            ValueFromRemainingArguments = $false, 
+            ParameterSetName = 'Default')]
+        [ValidateNotNull()]
+        [ValidateNotNullOrEmpty()]
+        $AccessPackageName = "B2B_ONboarding"
+
+    )
+    
+    begin {
+
+        Connect-MgGraph -Scopes "EntitlementManagement.ReadWrite.All"
+        Select-MgProfile -Name "beta"
+
+        if ($true -eq (Test-Path -Path $TextFilPath)) {
+            $TextData = get-content $TextFilePath
+
+        }
+        else {
+            New-Error  ("Text File {0} could not be found!  Please check the CSVPath!" -f $CsvPath)
+        }
+        
+    }
+    
+    process {
+
+        Write-Verbose ("{0} Rows in the Text File." -f $($TextData | Measure-Object -count | Select-Object -ExpandProperty count))
+
+        foreach ($EnailAddress in $TextData) {
+            $accesspackage = Get-MgEntitlementManagementAccessPackage -DisplayNameEq $AccessPackageName -ExpandProperty "accessPackageAssignmentPolicies"
+            $policy = $accesspackage.AccessPackageAssignmentPolicies[0]
+
+            try {
+            
+                Write-Verbose ("Assigning {0} to Access Package!")
+                $req = New-MgEntitlementManagementAccessPackageAssignmentRequest -AccessPackageId $accesspackage.Id -AssignmentPolicyId $policy.Id -TargetEmail $EmailAddress
+            }
+            catch {
+                Write-Error ("Error Assigning {0} to Access Package: {1}" -f $EmailAddress, $_.ErrorDetails)
+            }
+            
+        }
+    }
+    
+    end {
+        Write-Verbose "Complete!"
+    }
+}
