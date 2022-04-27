ded1cbdf8dd49858d498e6d95b2321b582e1f6c4
Jef Kazimer <jef@jeftek.com>
2022-04-27 09:31:16 -0500

    Update Tests

--- PowerShell/IdentityGovernance/AccessPackageAssignmentExpiration/Update-EMAccessPackageAssignmentExpiration.Tests.ps1
+++ PowerShell/IdentityGovernance/AccessPackageAssignmentExpiration/Update-EMAccessPackageAssignmentExpiration.Tests.ps1
@@ -1,4 +1,4 @@
-. .\AccessPackageAssignmentExpiration\Update-EMAccessPackageAssignmentExpiration.ps1
+. .\Update-EMAccessPackageAssignmentExpiration.ps1
 
 Describe InScopeDate {
     It 'Should return Correct Expiration endDateTime' {
@@ -11,6 +11,12 @@ Describe InScopeDate {
     It 'Attempting to set assignment to not expire should throw exception' {
         $ExceptionMessage = "RuntimeException: Updating an expiring assignment to no expiration is not currently supported!"
         Update-EMAccessPackageAssignmentExpiration -AccessPackageAssignmentId af31c660-5503-4b6e-a2c3-f6d2a7ed145f -SetNoExpiration | should -Throw -ExpectedMessage $ExceptionMessage
+
+    }
+
+    It 'Attempting to set assignment to timespan outside of the assignment policy timespan should throw exception' {
+        $ExpirationDate = ((get-date -AsUTC).ToUniversalTime()).AddDays(1000)
+        Update-EMAccessPackageAssignmentExpiration -AccessPackageAssignmentId af31c660-5503-4b6e-a2c3-f6d2a7ed145f -ExpirationDateTime $ExpirationDate | should -Throw
 
     }
 
